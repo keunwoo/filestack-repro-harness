13a67c3d7b1f946c37589054bbbfb2e130e82179
Keunwoo Lee <keunwoo@airtable.com>
2019-04-24 10:11:55 -0700

    add more upload sources

--- client.js
+++ client.js
@@ -3,6 +3,24 @@ const client = filestack.init(API_KEY);
 const pickButton = document.getElementById('pick');
 pickButton.addEventListener('click', () => {
     client.pick({
+        fromSources: [
+            'local_file_system',
+            'url',
+            'imagesearch',
+            'video',
+            'webcam',
+            'googledrive',
+            'dropbox',
+            'instagram',
+            'picasa',
+            'gmail',
+            'facebook',
+            'box',
+            'onedrive',
+            'onedriveforbusiness',
+            'flickr',
+            'evernote',
+        ],
         maxFiles: 1,
         uploadInBackground: false,
         onFileSelected: (file) => {
