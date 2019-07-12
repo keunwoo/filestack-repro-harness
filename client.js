097391a830b247a7d5a702ec4dbd0ef82d46d69d
Keunwoo Lee <keunwoo@airtable.com>
2019-07-12 10:32:48 -0700

    demonstrate `uploadInBackground` error

--- client.js
+++ client.js
@@ -3,23 +3,8 @@ const client = filestack.init(API_KEY);
 const pickButton = document.getElementById('pick');
 pickButton.addEventListener('click', () => {
     const picker = client.picker({
-        fromSources: [
-            'local_file_system',
-            'url',
-            'imagesearch',
-            'video',
-            'webcam',
-            'googledrive',
-            'dropbox',
-            'instagram',
-            'picasa',
-            'gmail',
-            'facebook',
-            'box',
-            'onedrive',
-            'onedriveforbusiness',
-        ],
-        maxFiles: 5,
+        fromSources: ['local_file_system'],
+        uploadInBackground: false,
     });
     picker.open();
 });
